2e1523026e217c3ca73ca979e64a7c60527afa6d
actzipild <mihairusu@gmail.com>
2017-02-24 15:44:23 +0200

    Played with the colors

--- app/src/main/java/ro/imbold/mrusu/awsomecalc/CalcActivity.java
+++ app/src/main/java/ro/imbold/mrusu/awsomecalc/CalcActivity.java
@@ -1,9 +1,9 @@
 package ro.imbold.mrusu.awsomecalc;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class CalcActivity extends AppCompatActivity {
+public class CalcActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
